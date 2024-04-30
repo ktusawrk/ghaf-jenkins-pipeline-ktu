ef51015e0c27896f8432d12a9e8d1e103ff06e3f
Kai Tusa <109805356+ktusawrk@users.noreply.github.com>
2024-04-30 16:38:17 +0300

    Full fmo

--- ghaf-build-pipeline.groovy
+++ ghaf-build-pipeline.groovy
@@ -39,12 +39,12 @@ pipeline {
     stage('Build on x86_64') {
       steps {
         dir('fmo') {
-          sh 'nix build --accept-flake-config github:tiiuae/FMO-OS#fmo-os-rugged-laptop-7330-public-debug -o result-rugged-laptop-7330-public-debug'
-//          sh 'nix build github:tiiuae/FMO-OS#fmo-os-rugged-laptop-7330-public-release'
-//          sh 'nix build github:tiiuae/FMO-OS#fmo-os-rugged-tablet-7230-public-debug'
-//          sh 'nix build github:tiiuae/FMO-OS#fmo-os-rugged-tablet-7230-public-release'
-//          sh 'nix build github:tiiuae/FMO-OS#fmo-os-installer-public-debug'
-//          sh 'nix build github:tiiuae/FMO-OS#fmo-os-installer-public-release'
+          sh 'nix build --accept-flake-config github:tiiuae/FMO-OS#fmo-os-rugged-laptop-7330-public-debug -o result-fmo-os-rugged-laptop-7330-public-debug'
+          sh 'nix build --accept-flake-config github:tiiuae/FMO-OS#fmo-os-rugged-laptop-7330-public-release -o result-fmo-os-rugged-laptop-7330-public-release'
+          sh 'nix build --accept-flake-config github:tiiuae/FMO-OS#fmo-os-rugged-tablet-7230-public-debug -o result-fmo-os-rugged-tablet-7230-public-debug'
+          sh 'nix build --accept-flake-config github:tiiuae/FMO-OS#fmo-os-rugged-tablet-7230-public-release -o result-fmo-os-rugged-tablet-7230-public-release'
+          sh 'nix build --accept-flake-config github:tiiuae/FMO-OS#fmo-os-installer-public-debug -o result-fmo-os-installer-public-debug'
+          sh 'nix build --accept-flake-config github:tiiuae/FMO-OS#fmo-os-installer-public-release -o result-fmo-os-installer-public-release'
         }
       }
     }
@@ -52,6 +52,11 @@ pipeline {
       steps {
         dir('fmo') {
           sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#fmo-os-rugged-laptop-7330-public-debug --csv result-fmo-os-rugged-laptop-7330-public-debug.csv --cdx result-fmo-os-rugged-laptop-7330-public-debug.cdx.json --spdx result-fmo-os-rugged-laptop-7330-public-debug.spdx.json'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#fmo-os-rugged-laptop-7330-public-release --csv result-fmo-os-rugged-laptop-7330-public-release.csv --cdx result-fmo-os-rugged-laptop-7330-public-release.cdx.json --spdx result-fmo-os-rugged-laptop-7330-public-release.spdx.json'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#fmo-os-rugged-tablet-7230-public-debug --csv result-fmo-os-rugged-tablet-7230-public-debug.csv --cdx result-fmo-os-rugged-tablet-7230-public-debug.cdx.json --spdx result-fmo-os-rugged-tablet-7230-public-debug.spdx.json'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#fmo-os-rugged-tablet-7230-public-release --csv result-fmo-os-rugged-tablet-7230-public-release.csv --cdx result-fmo-os-rugged-tablet-7230-public-release.cdx.json --spdx result-fmo-os-rugged-tablet-7230-public-release.spdx.json'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#fmo-os-installer-public-debug --csv result-fmo-os-installer-public-debug.csv --cdx result-fmo-os-installer-public-debug.cdx.json --spdx result-fmo-os-installer-public-debug.spdx.json'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#sbomnix -- .#fmo-os-installer-public-release --csv result-fmo-os-installer-public-release.csv --cdx result-fmo-os-installer-public-release.cdx.json --spdx result-fmo-os-installer-public-release.spdx.json'
         }
       }
     }
@@ -59,6 +64,11 @@ pipeline {
       steps {
         dir('fmo') {
           sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#vulnxscan -- .#fmo-os-rugged-laptop-7330-public-debug --out result-vulns-fmo-os-rugged-laptop-7330-public-debug.csv'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#vulnxscan -- .#fmo-os-rugged-laptop-7330-public-release --out result-vulns-fmo-os-rugged-laptop-7330-public-release.csv'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#vulnxscan -- .#fmo-os-rugged-tablet-7230-public-debug --out result-vulns-fmo-os-rugged-tablet-7230-public-debug.csv'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#vulnxscan -- .#fmo-os-rugged-tablet-7230-public-release --out result-vulns-fmo-os-rugged-tablet-7230-public-release.csv'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#vulnxscan -- .#fmo-os-installer-public-debug --out result-vulns-fmo-os-installer-public-debug.csv'
+          sh 'nix run github:tiiuae/sbomnix/a1f0f88d719687acedd989899ecd7fafab42394c#vulnxscan -- .#fmo-os-installer-public-release --out result-vulns-fmo-os-installer-public-release.csv'
         }
       }
     }
